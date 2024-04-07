f59f3d826e9cc3d5b634a575564558719632af9c
Mark Hahn <mark@hahnca.com>
2024-04-06 21:57:08 -0700

    auth cleanup

--- src/emby.js
+++ src/emby.js
@@ -1,20 +1,23 @@
 import axios from "axios"
 
+const name      = "mark";
+const pwd       = "90-MNBbnmyui";
+const apiKey    = "1c399bd079d549cba8c916244d3add2b"
 const markUsrId = "894c752d448f45a3a1260ccaabd0adff";
-const authHdr = 'UserId="894c752d448f45a3a1260ccaabd0adff", ' +
-                'Client="MyClient", Device="myDevice", '      +
-                'DeviceId="123456", Version="1.0.0"';
+const authHdr   = 'UserId="894c752d448f45a3a1260ccaabd0adff", ' +
+                  'Client="MyClient", Device="myDevice", '      +
+                  'DeviceId="123456", Version="1.0.0"';
 let token = '';
 
 
 ////////////////////////  INIT  ///////////////////////
 
-const getToken = async (name, pwd) => {
+const getToken = async () => {
   const config = {
     method: 'post',
     url: "http://hahnca.com:8096" +
          "/emby/Users/AuthenticateByName" +
-         "?api_key=1c399bd079d549cba8c916244d3add2b",
+         `?api_key=${apiKey}`,
     headers: { Authorization: authHdr },
     data: { Username: name, Pw: pwd },
   };
@@ -23,7 +26,7 @@ const getToken = async (name, pwd) => {
 }
 
 export async function init() {
-  await getToken('mark', '90-MNBbnmyui');
+  await getToken();
 }
 
 
